05e8da78f64c4c440a756ac99718ec1d9febdeff
Vortexgenial <michfrancogalardon@gmail.com>
2024-09-09 18:09:41 -0600

    Update guardar_datos.php

--- guardar_datos.php
+++ guardar_datos.php
@@ -1,7 +1,7 @@
 <!--
-la linea refresh, le indica al navegador dentro de cuantos seguundos (en este caso cero que significa instantaneamente) se ira a tal url (en este caso iperfil.html del mismo directorio)
+la linea refresh, le indica al navegador dentro de cuantos seguundos (en este caso cero que significa instantaneamente) se ira a tal url (en este caso iVortex👽.html del mismo directorio)
 !-->
-<meta http-equiv="refresh" content="0;url=iperfil.html">
+<meta http-equiv="refresh" content="0;url=iVortex👽.html">
 
 <?php
 //Credenciales
